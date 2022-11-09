af9a6c11e9a7460a9127cf57d0849086ac36fd34
Jérôme Vouillon <jerome.vouillon@gmail.com>
2022-11-09 17:49:18 +0100

    WIP

--- compiler/lib/lambda_lifting.ml
+++ compiler/lib/lambda_lifting.ml
@@ -1,18 +1,7 @@
 (*
-level of all variables
-===> level 0 are not touched; free variable if less than function level
-
-
-lift functions that are too deep and not in mutual recursion
-
-traverse function to find free variables
-return function + substitution
-
-
-g = function (freevars) { function f (params) { body }}
-
-function f (params) {body} -->
-    apply g (freevars)
+- clean-up
+- timer
+- baseline is not 0
 *)
 open Code
 
