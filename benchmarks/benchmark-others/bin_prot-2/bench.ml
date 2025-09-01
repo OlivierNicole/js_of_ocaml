54aaaf40eed0afda76a6b56c6a88b898b0f2268f
Olivier Nicole <olivier@chnik.fr>
2025-09-01 17:58:47 +0200

    wip: Made benchmark work

--- benchmarks/benchmark-others/bin_prot-2/bench.ml
+++ benchmarks/benchmark-others/bin_prot-2/bench.ml
@@ -1,3 +1,2 @@
-(* This module is intentionally empty *)
 let () =
-  Inline_benchmarks_public.Runner.main ~libname:"bin_prot_bench"
+  Inline_benchmarks_js_public.Runner.main ~libname:"bin_prot_bench"
